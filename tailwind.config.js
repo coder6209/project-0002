17e26fdc074b08dbb431cfd409a8ec424c174af5
Ilyas Husain <168177860+code6600@users.noreply.github.com>
2024-06-14 02:06:01 +0300

    Add design style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,28 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dodger-blue': {
+          '50': '#eff8ff',
+          '100': '#def0ff',
+          '200': '#b6e2ff',
+          '300': '#75ccff',
+          '400': '#2cb3ff',
+          '500': '#0aa1ff',
+          '600': '#0078d4',
+          '700': '#005fab',
+          '800': '#00518d',
+          '900': '#064474',
+          '950': '#042b4d',
+        },
+      }
+    },
   },
   plugins: [],
-}
+};
