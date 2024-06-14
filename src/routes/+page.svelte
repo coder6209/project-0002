160e68988e4f5f82211f4e2e3d4be77a5f35bfe4
Ilyas Husain <168177860+code6600@users.noreply.github.com>
2024-06-14 09:02:18 +0300

    New designs

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,17 @@
-<h1 class="text-red-500 font-bold">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="bg-spring-wood-50 layout flex-col min-h-screen w-full gap-0 p-0">
+  <div class="bg-white shadow-spring-wood-100 shadow-xl rounded-2xl layout flex-col w-11/12 max-w-sm mb-3 gap-12 p-12">
+    <div class="layout flex-col gap-0 p-0">
+      <h3>Ilyasco</h3>
+      <p class="opacity-50">Minimalist todo app</p>
+    </div>
+
+    <form class="layout flex-col w-full p-0">    
+      <input class="bg-gray-100 rounded-xl layout w-full hover:opacity-80 transition-all px-5" type="email" placeholder="Email" required/>
+      <input class="bg-gray-100 rounded-xl layout w-full hover:opacity-80 transition-all px-5" type="password" placeholder="Password" required/>
+      <button class="button-black w-full" type="submit">Wonder</button>  
+    </form>
+
+    <button class="button-border-black w-full" type="button">GitHub</button>
+  </div>
+  <p class="underline">Need an account?</p>
+</div>
